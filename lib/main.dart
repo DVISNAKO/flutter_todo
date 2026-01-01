6f0b19924d5575e6fc11a05e0e18fcf72ce809da
DVISNAKO <dmitrijs.visnakovs@inbox.lv>
2026-01-01 11:09:42 +0200

    bottomNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/home_page.dart';
+import 'package:todo_app/pages/menu_page.dart';
 
-
-void main(){
+void main() {
   runApp(const Myapp());
 }
 
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
   const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [const HomePage(), const MenuPage()];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(child: HomePage()),
+        body: pages[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Menu"),
+          ],
+        ),
       ),
     );
   }
